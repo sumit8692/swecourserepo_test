174cbd4c3628600a833c063129a8efbae05342ba
Ananya Singh <ananyasingh@ananyas-air.mynetworksettings.com>
2026-03-15 01:13:51 -0400

    test4

--- treestagram_project/treestagram/settings.py
+++ treestagram_project/treestagram/settings.py
@@ -232,3 +232,13 @@ EMAIL_HOST_USER = 'rgqu kqfe capo wqsa'
 EMAIL_HOST_PASSWORD = 'ananyasingh180600@gmail.com'
 
 DEFAULT_FROM_EMAIL = 'Treestagram <>'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+if AWS_STORAGE_BUCKET_NAME:
+ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
